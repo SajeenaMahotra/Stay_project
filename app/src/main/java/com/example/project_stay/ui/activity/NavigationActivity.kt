deab4199ec00eb89ed26cd6cbb57c680d4144ede
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2024-12-31 21:37:03 +0545

    commit navbar

---
+++ app/src/main/java/com/example/project_stay/ui/activity/NavigationActivity.kt
@@ -0,0 +1,60 @@
+package com.example.project_stay.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.project_stay.R
+import com.example.project_stay.databinding.ActivityLoginBinding
+import com.example.project_stay.databinding.ActivityNavigationBinding
+import com.example.project_stay.ui.fragment.BookingFragment
+import com.example.project_stay.ui.fragment.HomeFragment
+import com.example.project_stay.ui.fragment.ProfileFragment
+import com.example.project_stay.ui.fragment.WishlistFragment
+
+class NavigationActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNavigationBinding
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager: FragmentManager =supportFragmentManager
+        val fragmentTransaction :FragmentTransaction =fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameDashboard,fragment)
+        fragmentTransaction.commit()
+    }
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding =ActivityNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+        binding.bottomNavbar.setOnItemSelectedListener { menu ->
+            when(menu.itemId){
+                R.id.navHome -> replaceFragment(HomeFragment())
+                    R.id.navFavorite -> replaceFragment(WishlistFragment())
+                        R.id.navBookings -> replaceFragment(BookingFragment())
+                            R.id.navProfile -> replaceFragment(ProfileFragment())
+                                else ->{}
+            }
+            true
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+    }
+}
